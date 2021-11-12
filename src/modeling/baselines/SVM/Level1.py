5279d0b99e49774f15521fcfda3813807b6420cb
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-11-12 09:49:46 +0100

    add SVM

--- src/modeling/baselines/SVM/Level1.py
+++ src/modeling/baselines/SVM/Level1.py
@@ -1,8 +1,7 @@
 from src.preparation.training_data import TrainingData
 from src.modeling.baselines.SVM.svm_classifier import SVMClassifier
 
-## Level 1
-# create data
+
 kldb_level_1 = TrainingData(
     kldbs_path="data/raw/dictionary_occupations_complete_update.json",
     data_path="data/raw/2021-10-22_12-21-00_all_jobs_7.json",
