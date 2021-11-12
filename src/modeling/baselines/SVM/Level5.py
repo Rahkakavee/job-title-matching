a0278e76f0b619b42d279bc4d64b0f54e09e3849
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-11-12 09:44:15 +0100

    add SVM classifier

---
+++ src/modeling/baselines/SVM/Level5.py
@@ -0,0 +1,18 @@
+from src.preparation.training_data import TrainingData
+from src.modeling.baselines.SVM.svm_classifier import SVMClassifier
+
+## Level 1
+# create data
+kldb_level_5 = TrainingData(
+    kldbs_path="data/raw/dictionary_occupations_complete_update.json",
+    data_path="data/raw/2021-10-22_12-21-00_all_jobs_7.json",
+    kldb_level=5,
+)
+kldb_level_5.create_training_data()
+
+clf = SVMClassifier(data=kldb_level_5.training_data, vectorizer="CountVectorizer")
+
+clf.train_classifier()
+clf.evaluate(output_dict=False)
+print(clf.accuracy)
+print(clf.classfication_report)
---
+++ src/modeling/baselines/SVM/svm_classifier.py
@@ -0,0 +1,90 @@
+from typing import Dict, Union, List
+from numpy import vectorize
+import pandas as pd
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+
+class SVMClassifier:
+    def __init__(self, data: Union[List, Dict], vectorizer: str) -> None:
+        """init
+
+        Parameters
+        ----------
+        data : Union[List, Dict]
+            data with text data and labels
+        """
+        self.dataset = data
+        self.vectorizer = vectorizer
+
+    def vectorize_data(self):
+        """vectorize text data
+
+        Returns
+        -------
+        [type]
+            vectorized text data
+        """
+        self.df = pd.DataFrame(data=self.dataset)
+        if self.vectorizer == "CountVectorizer":
+            vectorizer = CountVectorizer()
+            vectorizer.fit(self.df["title"])
+            # data = vectorizer.fit_transform(self.df["title"]).toarray()
+            data = vectorizer.transform(self.df["title"])
+        if self.vectorizer == "TfidfVectorizer":
+            vectorizer = TfidfVectorizer()
+            data = vectorizer.fit_transform(self.df["title"]).toarray()
+        return data
+
+    def extract_labels(self):
+        """extract labels
+
+        Returns
+        -------
+        [type]
+            labels (classes)
+        """
+        labels = self.df.iloc[:, 0]
+        return labels
+
+    def split_data(self, data, labels):
+        """split data into training and test data
+
+        Parameters
+        ----------
+        data : [type]
+            text data (vectorized)
+        labels : [type]
+            labels
+        """
+        (
+            self.data_train,
+            self.data_test,
+            self.label_train,
+            self.label_test,
+        ) = train_test_split(data, labels)
+
+    def train_classifier(self):
+        """trains classfier"""
+        print("BEGIN")
+        print("Vectorize Data...")
+        data = self.vectorize_data()
+        print("Extract Labels...")
+        labels = self.extract_labels()
+        print("Split TrainingData...")
+        self.split_data(data=data, labels=labels)
+        print("Train the classfier")
+        self.svm_classifier = svm.SVC(C=1.0, kernel="linear", degree=3, gamma="auto")
+        self.svm_classifier.fit(self.data_train, self.label_train)
+        print("END")
+
+    def evaluate(self, output_dict: bool):
+        """evaluate data"""
+        self.accuracy = self.svm_classifier.score(self.data_test, self.label_test)
+        self.classfication_report = metrics.classification_report(
+            self.label_test,
+            self.svm_classifier.predict(self.data_test),
+            output_dict=output_dict,
+        )
