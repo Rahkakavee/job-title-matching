8dd5617984b005038d3a5b50e01220140315210d
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-11-27 22:46:46 +0100

    create Level 1 baseline training

---
+++ src/Level1_Baseline.py
@@ -0,0 +1,116 @@
+from src.preprocessing.preprocessing_functions import *
+from src.preprocessing.training_data import TrainingData
+from src.modeling.baselines.SVM.svm_classifier import SVMClassifier
+from src.modeling.baselines.naive_bayes.bayes_classifier import BayesClassifier
+from src.modeling.baselines.LR.lr_classifier import LRClassifier
+import pandas as pd
+from src.logger import logger
+import pickle
+
+logger.debug("#######TRAINING DATA#######")
+# Training Data
+data_level_1_old = TrainingData(
+    kldbs_path="data/raw/dictionary_occupations_complete_update.json",
+    data_path="data/processed/data_old_format.json",
+    kldb_level=1,
+    new_data=False,
+)
+data_level_1_new = TrainingData(
+    kldbs_path="data/raw/dictionary_occupations_complete_update.json",
+    data_path="data/processed/data_new_format.json",
+    kldb_level=1,
+    new_data=True,
+)
+
+data_level_1_old.create_training_data()
+data_level_1_new.create_training_data()
+
+training_data_level_1 = data_level_1_old.training_data + data_level_1_new.training_data
+
+data = [
+    dict(t) for t in {tuple(example.items()) for example in training_data_level_1}
+]  # source: "https://stackoverflow.com/questions/9427163/remove-duplicate-dict-in-list-in-python"
+
+with open("src/preprocessing/specialwords.tex", "rb") as fp:
+    specialwords = pickle.load(fp)
+
+logger.debug("#######Preprocessing#######")
+# Preprocess
+training_data = preprocess(
+    data=data, lowercase_whitespace=False, special_words_ovr=specialwords
+)
+
+# logger.debug("#######SVM COUNTVECTORIZER#######")
+# # Train SVM classifier with TfidfVectorizer
+# SVM_CountVectorizer_clf = SVMClassifier(
+#     data=training_data, vectorizer="CountVectorizer"
+# )
+# SVM_CountVectorizer_clf.train_classifier()
+# SVM_CountVectorizer_clf.evaluate(output_dict=True)
+# print(SVM_CountVectorizer_clf.accuracy)
+# print(SVM_CountVectorizer_clf.classfication_report)
+# df = pd.DataFrame(SVM_CountVectorizer_clf.classfication_report).transpose()
+# print(df.to_latex())
+
+# logger.debug("#######SVM TF-IDF")
+# # Train SVM classifier with TfidfVectorizer
+# SVM_TfidfVectorizer_clf = SVMClassifier(
+#     data=training_data, vectorizer="TfidfVectorizer"
+# )
+# SVM_TfidfVectorizer_clf.train_classifier()
+# SVM_TfidfVectorizer_clf.evaluate(output_dict=True)
+# print(SVM_TfidfVectorizer_clf.accuracy)
+# print(SVM_TfidfVectorizer_clf.classfication_report)
+# df = pd.DataFrame(SVM_TfidfVectorizer_clf.classfication_report).transpose()
+# print(df.to_latex())
+
+
+# # Save SVM classifiers
+# with open("model/SVM_CountVectorizer_clf_level1.pkl", "wb") as f:
+#     pickle.dump(SVM_CountVectorizer_clf, f)
+# with open("model/SVM_TfidfVectorizer_clf_level1.pkl", "wb") as f:
+#     pickle.dump(SVM_TfidfVectorizer_clf, f)
+
+logger.debug("#######NB COUNTVECTORIZER#######")
+# Train Bayes classifier with CountVectorizer
+NB_CountVectorizer_clf = BayesClassifier(data=data, vectorizer="CountVectorizer")
+NB_CountVectorizer_clf.train_classifier()
+NB_CountVectorizer_clf.evaluate(output_dict=False)
+print(NB_CountVectorizer_clf.classfication_report)
+
+logger.debug("#######NB TF-IDF#######")
+# Train Bayes classifier with TfidfVectorizer
+NB_TfidfVectorizer_clf = BayesClassifier(data=data, vectorizer="CountVectorizer")
+NB_TfidfVectorizer_clf.train_classifier()
+NB_TfidfVectorizer_clf.evaluate(output_dict=False)
+print(NB_TfidfVectorizer_clf.classfication_report)
+
+# Save NB classifiers
+with open("model/NB_CountVectorizer_clf_level1.pkl", "wb") as f:
+    pickle.dump(NB_CountVectorizer_clf, f)
+with open("model/SVM_clf_TfidfVectorizer_level1.pkl", "wb") as f:
+    pickle.dump(NB_TfidfVectorizer_clf, f)
+
+logger.debug("#######LR COUNTVECTORIZER#######")
+# Train LR classifier with CountVectorizer
+LR_CountVectorizer_clf = LRClassifier(data=data, vectorizer="CountVectorizer")
+LR_CountVectorizer_clf.train_classifier()
+LR_CountVectorizer_clf.evaluate(output_dict=False)
+print(LR_CountVectorizer_clf.classfication_report)
+
+logger.debug("#######LR TF-IDF#######")
+# Train LR classifier with TfidfVectorizer
+LR_TfidfVectorizer_clf = LRClassifier(data=data, vectorizer="TfidfVectorizer")
+LR_TfidfVectorizer_clf.train_classifier()
+LR_TfidfVectorizer_clf.evaluate(output_dict=False)
+print(LR_TfidfVectorizer_clf.classfication_report)
+
+# Save NB classifiers
+with open("model/LR_CountVectorizer_clf_level1.pkl", "wb") as f:
+    pickle.dump(LR_CountVectorizer_clf, f)
+with open("model/LR_TfidfVectorizer_clf_level1.pkl", "wb") as f:
+    pickle.dump(LR_TfidfVectorizer_clf, f)
+
+
+# with open('filename.pkl', 'rb') as f:
+#     clf = pickle.load(f)
