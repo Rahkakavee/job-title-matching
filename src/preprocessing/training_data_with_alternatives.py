4cd1ecbd3a86582fbd1caa9fd2589bbfb1ff66e7
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-12-02 12:28:04 +0100

    analysis training alternatives

--- src/preprocessing/training_data_with_alternatives.py
+++ src/preprocessing/training_data_with_alternatives.py
@@ -1,10 +1,22 @@
 import json
+from collections import Counter
+import pandas as pd
+import re
+import itertools
+from src.preprocessing.preprocessing_functions import *
+import pickle
+from matplotlib import pyplot
+import seaborn as sns
+from src.descriptive.descriptive_analysis.descriptive_analysis_functions import *
 
+
+# load data
 with open(
     file="data/processed/data_old_format.json", mode="r", encoding="utf-8"
 ) as file:
     json_dict = json.load(fp=file)
 
+# load kldbs
 with open(
     file="data/raw/dictionary_occupations_complete_update.json",
     mode="r",
@@ -12,6 +24,7 @@ with open(
 ) as file:
     kldbs = json.load(fp=file)
 
+# get relevant data
 data_old = [job for job in json_dict if "freieBezeichnung" in job.keys()]
 
 data_old = [
@@ -24,19 +37,20 @@ data_old = [
     if "alternativDkzs" in job.keys()
 ]
 
+# create lookup table
 kldbs_dkzs = {}
 kldb_level5 = [kldb for kldb in kldbs if kldb["level"] == 5]
 for kldb in kldb_level5:
     for dkz in kldb["dkzs"]:
         kldbs_dkzs.update({str(dkz["id"]): kldb["id"]})
 
-
-training_data = []
+# match with kldbs
+data = []
 for job in data_old:
     try:
         alternative_kldb = []
         job_kldb = kldbs_dkzs[job["hauptDkz"]]
-        training_data.append(
+        data.append(
             {
                 "title": job["title"],
                 "hauptKldB": job_kldb,
@@ -46,20 +60,25 @@ for job in data_old:
     except:
         pass
 
+# preprocess data
+with open(file="src/preprocessing/specialwords.tex", mode="rb") as fp:
+    specialwords = pickle.load(fp)
+training_data = preprocess(data=data, special_words_ovr=specialwords)
 
-training_data_alternatives = []
-for job in training_data:
-    alternative_kldbs = []
-    for alternative in job["alternativDkzs"]:
-        try:
-            alternative_kldb = kldbs_dkzs[alternative]
-            alternative_kldbs.append(alternative_kldb)
-        except:
-            pass
-    training_data_alternatives.append(
-        {
-            "title": training_data["title"],
-            "hauptKldB": training_data["hauptKldB"],
-            "alternativeDkzs": alternative_kldb,
-        }
+# extract data according to occupations and important parts
+servicekraft = [
+    job
+    for job in training_data
+    if re.search(
+        r"servicekraft|service kraft|service kräfte|servicekräfte", job["title"]
     )
+]
+servicekraft_6000 = [
+    example for example in servicekraft if example["hauptKldB"][:1] == "6"
+]
+
+plt_servicekraft = co_occurence_with_kldbs(
+    data=servicekraft_6000,
+    kldbs_dkzs=kldbs_dkzs,
+    title="Co-occurence for Servicekraft (only HauptkldB category 6 considered",
+)
