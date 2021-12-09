bec2c74aaf3d7e3c096b6bcefc4d67916beaeebd
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-12-09 17:26:11 +0100

    update doc2vec and word2vec

--- src/vectorizer/Doc2vec/doc2vec.py
+++ src/vectorizer/Doc2vec/doc2vec.py
@@ -1,3 +1,5 @@
+"This file trains doc2vec model with and without prior knowledge"
+
 import pickle
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
@@ -10,46 +12,75 @@ from sklearn.model_selection import train_test_split
 from src.preprocessing.preprocessing_functions import *
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
-with open(file="data/processed/training_data_long.json") as fp:
+with open(file="data/processed/training_data_long_l1.json") as fp:
     training_data_long = json.load(fp=fp)
 
-with open(file="data/processed/training_data_short.json") as fp:
+with open(file="data/processed/training_data_short_l1.json") as fp:
     training_data_short = json.load(fp=fp)
 
 with open(file="data/raw/dictionary_occupations_complete_update.json") as fp:
     kldbs = json.load(fp=fp)
 
-sentences_short = [job["title"] for job in training_data_short]
-labels_short = [job["id"] for job in training_data_short]
+# process data from BA data
 sentences_long = [job["title"] for job in training_data_long]
 labels_long = [job["id"] for job in training_data_long]
-
-sentences_short_tokens = [word_tokenize(sentence) for sentence in sentences_short]
 sentences_long_tokens = [word_tokenize(sentence) for sentence in sentences_long]
 
+# create prior knowledge
 prior_knowledge = []
 for kldb in kldbs:
     if kldb["level"] == 5:
         for searchword in kldb["searchwords"]:
             prior_knowledge.append({"title": searchword["name"]})
-
-with open(file="src/preprocessing/specialwords.tex", mode="rb") as fp:
+with open(file="src/preprocessing/specialwords.txt", mode="rb") as fp:
     specialwords = pickle.load(fp)
-
 prior_knowledge_cleaned = preprocess(
     data=prior_knowledge, special_words_ovr=specialwords
 )
-
 prior_knowledge_cleaned_sentences = [
     sentence["title"] for sentence in prior_knowledge_cleaned
 ]
-
 prior_knowledge_cleaned_tokens = [
     word_tokenize(sentence) for sentence in prior_knowledge_cleaned_sentences
 ]
 
+# create data for training
 embedding_data = prior_knowledge_cleaned_tokens + sentences_long_tokens
+embedding_data_tagged = [TaggedDocument(d, [i]) for i, d in enumerate(embedding_data)]
+sentences_long_tokens_tagged = [
+    TaggedDocument(d, [i]) for i, d in enumerate(sentences_long_tokens)
+]
 
-tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(embedding_data)]
+# create models
+model_without_prior = Doc2Vec(
+    sentences_long_tokens_tagged, vector_size=300, window=5, min_count=1, epochs=10
+)
+model_with_prior = Doc2Vec(
+    embedding_data_tagged, vector_size=300, window=5, min_count=1, epochs=10
+)
 
-model = Doc2Vec(tagged_data, vector_size=20, window=5, min_count=1, epochs=100)
+model_without_prior.save("src/vectorizer/Doc2vec/doc2_vec_without_prior")
+model_with_prior.save("src/vectorizer/Doc2vec/doc2_vec_with_prior")
+
+# TEST
+sentences_short = [job["title"] for job in training_data_short]
+labels_short = [job["id"] for job in training_data_short]
+sentences_short_tokens = [word_tokenize(sentence) for sentence in sentences_short]
+
+
+train, test, y_train, y_test = train_test_split(sentences_short_tokens, labels_short)
+
+
+train_vecs = [model_without_prior.infer_vector(sent) for sent in train]
+test_vecs = [model_without_prior.infer_vector(sent) for sent in test]
+clf = LogisticRegression(max_iter=1000)
+clf.fit(train_vecs, y_train)
+score = clf.score(test_vecs, y_test)
+print(score)
+
+train_vecs = [model_with_prior.infer_vector(sent) for sent in train]
+test_vecs = [model_with_prior.infer_vector(sent) for sent in test]
+clf = LogisticRegression(max_iter=1000)
+clf.fit(train_vecs, y_train)
+score = clf.score(test_vecs, y_test)
+print(score)
--- src/vectorizer/word2vec/word2vec.py
+++ src/vectorizer/word2vec/word2vec.py
@@ -1,101 +1,77 @@
 from gensim.models import KeyedVectors
+import json
+from nltk import word_tokenize
+from gensim.models import Word2Vec
+import pickle
+from src.preprocessing.preprocessing_functions import *
+from src.vectorizer.word2vec.average_word_2_vec import AverageWord2Vec
+import numpy as np
 
+
+# load google model
 model = KeyedVectors.load_word2vec_format(
     "model/GoogleNews-vectors-negative300.bin.gz", binary=True
 )
 
-
-import json
-from nltk import word_tokenize
-
-with open(file="data/processed/training_data_long.json") as fp:
+# get data
+with open(file="data/processed/training_data_long_l1.json") as fp:
+    training_data_long = json.load(fp=fp)
+with open(file="data/processed/training_data_short_l1.json") as fp:
     training_data_short = json.load(fp=fp)
+sentences_long = [el["title"] for el in training_data_long]
+sentences_long_tokenized = [word_tokenize(sent) for sent in sentences_long]
 
-with open(file="data/processed/training_data_short.json.json") as fp:
-    training_data = json.load(fp=fp)
+# get knowledgebase
+with open(file="data/raw/dictionary_occupations_complete_update.json") as fp:
+    kldbs = json.load(fp=fp)
+prior_knowledge = []
+for kldb in kldbs:
+    if kldb["level"] == 5:
+        for searchword in kldb["searchwords"]:
+            prior_knowledge.append({"title": searchword["name"]})
+with open(file="src/preprocessing/specialwords.txt", mode="rb") as fp:
+    specialwords = pickle.load(fp)
+prior_knowledge_cleaned = preprocess(
+    data=prior_knowledge, special_words_ovr=specialwords
+)
+prior_knowledge_cleaned_sentences = [
+    sentence["title"] for sentence in prior_knowledge_cleaned
+]
+prior_knowledge_cleaned_tokens = [
+    word_tokenize(sentence) for sentence in prior_knowledge_cleaned_sentences
+]
+
+embedding_sentences = prior_knowledge_cleaned_tokens + sentences_long_tokenized
 
 
-sentences = [el["title"].lower() for el in training_data_short]
-sentences_short = [el["title"].lower() for el in training_data]
-sentences_tokenized = [word_tokenize(sent) for sent in sentences]
+# without prior knowledge
+custom_model_without_prior_knowledge = Word2Vec(vector_size=300, min_count=1)
+custom_model_without_prior_knowledge.build_vocab(sentences_long_tokenized)
+training_example_count = custom_model_without_prior_knowledge.corpus_count
+custom_model_without_prior_knowledge.build_vocab(
+    [list(model.key_to_index.keys())], update=True
+)
 
-from gensim.models import Word2Vec
-
-custom_model = Word2Vec(vector_size=300, min_count=1)
-custom_model.build_vocab(sentences_tokenized)
-training_example_count = custom_model.corpus_count
-custom_model.build_vocab([list(model.key_to_index.keys())], update=True)
-import numpy as np
-
-custom_model.wv.vectors_lockf = np.ones(len(custom_model.wv))
-custom_model.wv.intersect_word2vec_format(
+custom_model_without_prior_knowledge.wv.vectors_lockf = np.ones(
+    len(custom_model_without_prior_knowledge.wv)
+)
+custom_model_without_prior_knowledge.wv.intersect_word2vec_format(
     "model/GoogleNews-vectors-negative300.bin.gz", binary=True, lockf=1.0
 )
-custom_model.train(sentences, total_examples=training_example_count, epochs=10)
+custom_model_without_prior_knowledge.train(
+    sentences_long_tokenized, total_examples=training_example_count, epochs=10
+)
+
+custom_model_without_prior_knowledge.save(
+    "src/vectorizer/word2vec/word2vec_without_prior"
+)
 
 
-class DocSim:
-    def __init__(self, w2v_model, stopwords=None):
-        self.w2v_model = w2v_model
-        self.stopwords = stopwords if stopwords is not None else []
+sentences_short = [el["title"] for el in training_data_short]
 
-    def vectorize(self, doc: str) -> np.ndarray:
-        """
-        Identify the vector values for each word in the given document
-        :param doc:
-        :return:
-        """
-        words = [w for w in doc.split(" ")]
-        word_vecs = []
-        for word in words:
-            try:
-                vec = self.w2v_model[word]
-                word_vecs.append(vec)
-            except KeyError:
-                # Ignore, if the word doesn't exist in the vocabulary
-                pass
-
-        # Assuming that document vector is the mean of all the word vectors
-        # PS: There are other & better ways to do it.
-        vector = np.mean(word_vecs, axis=0)
-        return vector
-
-    def _cosine_sim(self, vecA, vecB):
-        """Find the cosine similarity distance between two vectors."""
-        csim = np.dot(vecA, vecB) / (np.linalg.norm(vecA) * np.linalg.norm(vecB))
-        if np.isnan(np.sum(csim)):
-            return 0
-        return csim
-
-    def calculate_target(self, targets):
-        return [self.vectorize(target) for target in targets]
-
-    def calculate_similarity(
-        self, source_doc, target_docs=None, target_vecs=None, threshold=0
-    ):
-        """Calculates & returns similarity scores between given source document & all
-        the target documents."""
-        # if not target_docs:
-        #     return []
-
-        # if isinstance(target_docs, str):
-        #     target_docs = [target_docs]
-
-        source_vec = self.vectorize(source_doc)
-        results = []
-        for i in tqdm(range(0, len(target_docs))):
-            target_vec = target_vecs[i]
-            sim_score = self._cosine_sim(source_vec, target_vec)
-            if sim_score > threshold:
-                results.append({"score": sim_score, "doc": target_docs[i]})
-            # Sort results by score in desc order
-            results.sort(key=lambda k: k["score"], reverse=True)
-        return results
-
-
-ds = DocSim(custom_model.wv)
-training_vecs = ds.calculate_target(sentences_short)
-labels = [el["id"] for el in training_data]
+avvec = AverageWord2Vec(custom_model_without_prior_knowledge.wv)
+training_vecs = avvec.vectorize_all(sentences_short)
+labels = [el["id"] for el in training_data_short]
 
 
 full_training_vecs = []
@@ -105,11 +81,55 @@ for i in range(0, len(training_vecs)):
         full_training_vecs.append(training_vecs[i])
         full_labels.append(labels[i])
 
-
+# Test
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 train, test, y_train, y_test = train_test_split(full_training_vecs, full_labels)
 clf = LogisticRegression(max_iter=1000)
 clf.fit(train, y_train)
-clf.score(test, y_test)
+score = clf.score(test, y_test)
+print(score)
+
+# with prior knowldege
+custom_model_with_prior_knowledge = Word2Vec(vector_size=300, min_count=1)
+custom_model_with_prior_knowledge.build_vocab(embedding_sentences)
+training_example_count = custom_model_with_prior_knowledge.corpus_count
+custom_model_with_prior_knowledge.build_vocab(
+    [list(model.key_to_index.keys())], update=True
+)
+import numpy as np
+
+custom_model_with_prior_knowledge.wv.vectors_lockf = np.ones(
+    len(custom_model_with_prior_knowledge.wv)
+)
+custom_model_with_prior_knowledge.wv.intersect_word2vec_format(
+    "model/GoogleNews-vectors-negative300.bin.gz", binary=True, lockf=1.0
+)
+custom_model_with_prior_knowledge.train(
+    embedding_sentences, total_examples=training_example_count, epochs=10
+)
+
+custom_model_with_prior_knowledge.save("src/vectorizer/word2vec/word2vec_with_prior")
+
+sentences_short = [el["title"] for el in training_data_short]
+
+avvec = AverageWord2Vec(custom_model_with_prior_knowledge.wv)
+training_vecs = avvec.vectorize_all(sentences_short)
+labels = [el["id"] for el in training_data_short]
+
+
+full_training_vecs = []
+full_labels = []
+for i in range(0, len(training_vecs)):
+    if training_vecs[i].shape == (300,):
+        full_training_vecs.append(training_vecs[i])
+        full_labels.append(labels[i])
+
+# Test
+
+train, test, y_train, y_test = train_test_split(full_training_vecs, full_labels)
+clf = LogisticRegression(max_iter=1000)
+clf.fit(train, y_train)
+score = clf.score(test, y_test)
+print(score)
