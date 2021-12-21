503a678a359504fa4500d0c9832067a9ae9a52f4
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-12-21 14:45:25 +0100

    add k fold option

--- src/modeling/RF/randomforest.py
+++ src/modeling/RF/randomforest.py
@@ -1,5 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
+from sklearn.model_selection import RepeatedKFold, cross_validate
+from scipy import stats
+import numpy as np
 
 
 class RFClassifier:
@@ -23,3 +26,46 @@ class RFClassifier:
             output_dict=output_dict,
         )
         return classfication_report
+
+    def mean_cfi(self, result, metric):
+        alpha = 0.05
+        metric = result[metric]
+        df = len(metric) - 1  # degree of freedom
+        t_value = stats.t.ppf(1 - alpha / 2, df)
+        std_ = np.std(metric, ddof=1)
+        n = len(metric)
+
+        lower = np.mean(metric) - (t_value * std_ / np.sqrt(n))
+        upper = np.mean(metric) + (t_value * std_ / np.sqrt(n))
+
+        return lower, upper
+
+    def cross_validate(self):
+        self.clf = RandomForestClassifier(n_estimators=100, criterion="gini")
+
+        scorings = [
+            "accuracy",
+            "precision_macro",
+            "recall_macro",
+            "f1_macro",
+            "precision_micro",
+            "recall_micro",
+            "f1_micro",
+        ]
+
+        kfold = RepeatedKFold(n_splits=5, n_repeats=20)
+        scores = cross_validate(
+            estimator=self.clf, X=self.train, y=self.y_train, cv=kfold, scoring=scorings
+        )
+        results = []
+        for scoring in scorings:
+            metric = "test_" + scoring
+            lower, upper = self.mean_cfi(scores, metric)
+            results.append(
+                {
+                    "metric": scoring,
+                    "mean": np.mean(scores[metric]),
+                    "cfi": f"[{lower}, {upper}]",
+                }
+            )
+        return results
