f95e17df0774526cf5f3fdcb9ab96a15f19c5eee
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-12-21 14:42:11 +0100

    add k fold option

--- src/modeling/SVM/svm.py
+++ src/modeling/SVM/svm.py
@@ -2,6 +2,9 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import BaggingClassifier
 from sklearn import metrics
+from sklearn.model_selection import RepeatedKFold, cross_validate
+from scipy import stats
+import numpy as np
 
 
 class SVMClassifier:
@@ -37,3 +40,55 @@ class SVMClassifier:
             output_dict=output_dict,
         )
         return classfication_report
+
+    def mean_cfi(self, result, metric):
+        alpha = 0.05
+        metric = result[metric]
+        df = len(metric) - 1  # degree of freedom
+        t_value = stats.t.ppf(1 - alpha / 2, df)
+        std_ = np.std(metric, ddof=1)
+        n = len(metric)
+
+        lower = np.mean(metric) - (t_value * std_ / np.sqrt(n))
+        upper = np.mean(metric) + (t_value * std_ / np.sqrt(n))
+
+        return lower, upper
+
+    def cross_validate(self):
+        n_estimators = 10
+        self.clf = OneVsRestClassifier(
+            BaggingClassifier(
+                SVC(C=1.0, kernel="linear", gamma="scale"),
+                max_samples=1.0,
+                n_estimators=n_estimators,
+                n_jobs=-1,
+                verbose=True,
+            )
+        )
+
+        scorings = [
+            "accuracy",
+            "precision_macro",
+            "recall_macro",
+            "f1_macro",
+            "precision_micro",
+            "recall_micro",
+            "f1_micro",
+        ]
+
+        kfold = RepeatedKFold(n_splits=5, n_repeats=20)
+        scores = cross_validate(
+            estimator=self.clf, X=self.train, y=self.y_train, cv=kfold, scoring=scorings
+        )
+        results = []
+        for scoring in scorings:
+            metric = "test_" + scoring
+            lower, upper = self.mean_cfi(scores, metric)
+            results.append(
+                {
+                    "metric": scoring,
+                    "mean": np.mean(scores[metric]),
+                    "cfi": f"[{lower}, {upper}]",
+                }
+            )
+        return results
