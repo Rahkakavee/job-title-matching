f609d310d3f097e5c4d38af10907c85ec45ea52f
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2022-01-14 15:48:57 +0100

    final update class distribution

---
+++ src/descriptive/descriptive_analysis/class_dist_long_data .py
@@ -0,0 +1,31 @@
+# import
+from src.descriptive.descriptive_analysis.descriptive_analysis_functions import *
+import json
+
+with open(file="data/processed/training_data_long_l1.json") as fp:
+    training_data_short_l1 = json.load(fp=fp)
+
+with open(file="data/processed/training_data_long_l3.json") as fp:
+    training_data_short_l3 = json.load(fp=fp)
+
+with open(file="data/processed/training_data_long_l5.json") as fp:
+    training_data_short_l5 = json.load(fp=fp)
+
+
+# class_distribution, plt = class_distribution(
+#     data=training_data_short_l1, variable="id", level="1"
+# )
+# plt.savefig("visualization/descriptive_analysis/training_data_long_L1.png")
+
+
+class_distribution, plt = class_distribution(
+    data=training_data_short_l3, variable="id", level="3"
+)
+
+plt.savefig("visualization/descriptive_analysis//training_data_long_L3.png")
+
+
+# class_distribution, plt = class_distribution(
+#     data=training_data_short_l5, variable="id", level="5"
+# )
+# plt.savefig("visualization/descriptive_analysis//training_data_long_L5.png")
